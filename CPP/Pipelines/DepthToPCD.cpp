66306b5e2b980d4c3c14fdb5acb93f65d6f12c0f
liuzikai <zikai.liu@outlook.com>
2022-11-15 22:30:34 +0100

    Improve pipeline for depth processing
    Show conservative condition for both hand tracked.

--- CPP/Pipelines/DepthToPCD.cpp
+++ CPP/Pipelines/DepthToPCD.cpp
@@ -44,10 +44,24 @@ std::unique_ptr<FileDataSource> fileDataSource;
 
 TCPDataSource tcpStreamingSource;
 
+bool discardDelayedFrames = false;
+
 AHATSource *ahatSource = nullptr;
 InteractionSource *interactionSource = nullptr;
 
 std::unique_ptr<DepthProcessorWrapper> depthProcessor;
+
+int countTrackedJoints(const Hand &hand) {
+    int result = 0;
+    for (const auto &joint: hand.joints) {
+        if (joint.tracked) result++;
+    }
+    return result;
+}
+
+static const Eigen::RowVector3d HAND_COLOR[HandIndexCount] = {Eigen::RowVector3d(0, 1, 0),
+                                                              Eigen::RowVector3d(1, 0, 0)};
+Eigen::MatrixXd BOTH_HANDS_EDGE_COLORS(48, 3);
 
 bool callBackPerDraw(igl::opengl::glfw::Viewer &viewer) {
     // static to hold the latest data
@@ -66,30 +80,65 @@ bool callBackPerDraw(igl::opengl::glfw::Viewer &viewer) {
         std::cout << "Create DepthProcessor with AHAT extrinsics and LUT" << std::endl;
     }
 
-    bool updated = false;
+    bool redraw = false;
+    bool lostTracking = false;
 
-    timestamp_t ahatTimestamp;
-    AHATDepth depth;
-    DirectX::XMMATRIX rig2world;
-    while (ahatSource && ahatSource->getNextAHATFrame(ahatTimestamp, depth, rig2world)) {
-        /*std::cout << "[DEPTH] " << ahatTimestamp << std::endl;*/
-        depthProcessor->updateAHAT(ahatTimestamp, depth.data(), rig2world);
+    if (ahatSource) {
+        timestamp_t ahatTimestamp;
+        AHATDepth depth;
+        DirectX::XMMATRIX rig2world;
+
+        do {
+            if (!ahatSource->getNextAHATFrame(ahatTimestamp, depth, rig2world)) break;
+            // std::cout << "[DEPTH] " << ahatTimestamp << std::endl;
+            depthProcessor->updateAHAT(ahatTimestamp, depth.data(), rig2world);
+        } while (discardDelayedFrames);  // continue the loop if discardDelayedFrames
     }
 
-    while (depthProcessor->getNextPCD(pcdTimestamp, pcd)) {
-        /*std::cout << "[PCD] " << pcdTimestamp << std::endl;*/
-        updated = true;
+    if (depthProcessor) {
+        do {
+            if (!depthProcessor->getNextPCD(pcdTimestamp, pcd)) break;
+            // std::cout << "[PCD] " << pcdTimestamp << std::endl;
+//            redraw = true;
+// FIXME: not trigger update for now
+        } while (discardDelayedFrames);  // continue the loop if discardDelayedFrames
     }
 
-    while (interactionSource && interactionSource->getNextInteractionFrame(interationTimestamp, interactionFrame)) {
-        /*std::cout << "[INT] " << interationTimestamp
-                  << " Left Hand Tracked: " << interactionFrame.hands[Left].tracked
-                  << " Right Hand Tracked: " << interactionFrame.hands[Right].tracked
-                  << std::endl;*/
-        updated = true;
+    if (interactionSource) {
+        do {
+            timestamp_t newInterationTimestamp;
+            InteractionFrame newInteractionFrame;
+            if (!interactionSource->getNextInteractionFrame(newInterationTimestamp, newInteractionFrame)) break;
+            std::cout << "[INT] "
+                      << " L: " << newInteractionFrame.hands[Left].tracked << "/"
+                      << countTrackedJoints(newInteractionFrame.hands[Left])
+                      << " R: " << newInteractionFrame.hands[Right].tracked << "/"
+                      << countTrackedJoints(newInteractionFrame.hands[Right])
+                      << std::endl;
+            redraw = true;
+
+            if (newInteractionFrame.hands[Left].tracked && countTrackedJoints(newInteractionFrame.hands[Left]) == 0 ||
+                newInteractionFrame.hands[Right].tracked && countTrackedJoints(newInteractionFrame.hands[Right]) == 0) {
+                std::cout << "Discard frame on tracking" << std::endl;
+                lostTracking = true;
+                // should redraw but should not update interaction data
+            } else {
+                lostTracking = false;
+            }
+
+            if (!lostTracking) {
+                interationTimestamp = newInterationTimestamp;
+                interactionFrame = newInteractionFrame;
+            }
+
+        } while (discardDelayedFrames);  // continue the loop if discardDelayedFrames
     }
 
-    if (updated) {
+    bool leftTracked = interactionFrame.hands[Left].tracked && countTrackedJoints(interactionFrame.hands[Left]);
+    bool rightTracked = interactionFrame.hands[Right].tracked && countTrackedJoints(interactionFrame.hands[Right]);
+    if (!leftTracked || !rightTracked) lostTracking = true;
+
+    if (redraw) {
         viewer.data().clear();
 
         // PCD
@@ -97,52 +146,58 @@ bool callBackPerDraw(igl::opengl::glfw::Viewer &viewer) {
         for (int i = 0; i < pcd.size(); i++) {
             points.row(i) = pcd[i].cast<double>();
         }
-        viewer.data().add_points(points, Eigen::RowVector3d(1, 1, 1));
+        viewer.data().add_points(points,
+                                 lostTracking ? Eigen::RowVector3d(1, 0, 0) : Eigen::RowVector3d(1, 1, 1) * 0.8f);
 
         // Interaction
-        static const Eigen::RowVector3d handColors[HandIndexCount] = {Eigen::RowVector3d(0, 0, 1),
-                                                                      Eigen::RowVector3d(1, 0, 0)};
-        for (int i = 0; i < HandIndexCount; i++) {
-            if (!interactionFrame.hands[i].tracked) continue;
+        if (!lostTracking) {
+            if (leftTracked && rightTracked) {
+                // Both hands
 
-            Eigen::MatrixXd jointPoints((int) HandJointIndexCount, 3);
-            for (int j = 0; j < HandJointIndexCount; j++) {
-                jointPoints.row(j) = XMVectorToEigenVector3d(
-                        XMTransformToTranslate(interactionFrame.hands[i].joints[j].transformation));
+                static const Eigen::MatrixXi EDGES_BOTH_HANDS = (Eigen::MatrixXi(48, 2)
+                        <<
+                        /* Left  */
+                        /* Thumb  */ 1, 2, /**/ 2, 3, /**/ 3, 4, /**/ 4, 5,
+                        /* Index  */ 1, 6, /**/ 6, 7, /**/ 7, 8, /**/ 8, 9, /**/ 9, 10,
+                        /* Middle */ 1, 11, /**/ 11, 12, /**/ 12, 13, /**/ 13, 14, /**/ 14, 15,
+                        /* Ring   */ 1, 16, /**/ 16, 17, /**/ 17, 18, /**/ 18, 19, /**/ 19, 20,
+                        /* Pinky  */ 1, 21, /**/ 21, 22, /**/ 22, 23, /**/ 23, 24, /**/ 24, 25,
+                        /* Right  */
+                        /* Thumb  */ 27, 28, /**/ 28, 29, /**/ 29, 30, /**/ 30, 31,
+                        /* Index  */ 27, 32, /**/ 32, 33, /**/ 33, 34, /**/ 34, 35, /**/ 35, 36,
+                        /* Middle */ 27, 37, /**/ 37, 38, /**/ 38, 39, /**/ 39, 40, /**/ 40, 41,
+                        /* Ring   */ 27, 42, /**/ 42, 43, /**/ 43, 44, /**/ 44, 45, /**/ 45, 46,
+                        /* Pinky  */ 27, 47, /**/ 47, 48, /**/ 48, 49, /**/ 49, 50, /**/ 50, 51).finished();
+
+                Eigen::MatrixXd jointPoints((int) HandJointIndexCount * 2, 3);
+
+                for (int i = 0; i < HandIndexCount; i++) {
+                    for (int j = 0; j < HandJointIndexCount; j++) {
+                        jointPoints.row(i * HandJointIndexCount + j) = XMVectorToEigenVector3d(
+                                XMTransformToTranslate(interactionFrame.hands[i].joints[j].transformation));
+                    }
+                }
+                viewer.data().set_edges(jointPoints, EDGES_BOTH_HANDS, BOTH_HANDS_EDGE_COLORS);
+
+            } else if (leftTracked || rightTracked) {
+                // Single hand
+
+                int i = leftTracked ? Left : Right;
+                static const Eigen::MatrixXi EDGES_SINGLE_HAND = (Eigen::MatrixXi(24, 2)
+                        <<
+                        /* Thumb  */ 1, 2, /**/ 2, 3, /**/ 3, 4, /**/ 4, 5,
+                        /* Index  */ 1, 6, /**/ 6, 7, /**/ 7, 8, /**/ 8, 9, /**/ 9, 10,
+                        /* Middle */ 1, 11, /**/ 11, 12, /**/ 12, 13, /**/ 13, 14, /**/ 14, 15,
+                        /* Ring   */ 1, 16, /**/ 16, 17, /**/ 17, 18, /**/ 18, 19, /**/ 19, 20,
+                        /* Pinky  */ 1, 21, /**/ 21, 22, /**/ 22, 23, /**/ 23, 24, /**/ 24, 25).finished();
+
+                Eigen::MatrixXd jointPoints((int) HandJointIndexCount, 3);
+                for (int j = 0; j < HandJointIndexCount; j++) {
+                    jointPoints.row(j) = XMVectorToEigenVector3d(
+                            XMTransformToTranslate(interactionFrame.hands[i].joints[j].transformation));
+                }
+                viewer.data().set_edges(jointPoints, EDGES_SINGLE_HAND, HAND_COLOR[i]);
             }
-
-            static const Eigen::MatrixXi edgesBetweenJoints = (Eigen::MatrixXi(24, 2) << 1, 2,
-                    2, 3,
-                    3, 4,
-                    4, 5,
-
-                    1, 6,
-                    6, 7,
-                    7, 8,
-                    8, 9,
-                    9, 10,
-
-                    1, 11,
-                    11, 12,
-                    12, 13,
-                    13, 14,
-                    14, 15,
-
-                    1, 16,
-                    16, 17,
-                    17, 18,
-                    18, 19,
-                    19, 20,
-
-                    1, 21,
-                    21, 22,
-                    22, 23,
-                    23, 24,
-                    24, 25).finished();
-
-//            std::cout << jointPoints << std::endl;
-
-            viewer.data().set_edges(jointPoints, edgesBetweenJoints, handColors[i]);
         }
 
         // Set camera on first frame
@@ -157,20 +212,30 @@ bool callBackPerDraw(igl::opengl::glfw::Viewer &viewer) {
 }
 
 int main() {
+    std::ios::sync_with_stdio(false);
 
+    discardDelayedFrames = true;
     ahatSource = static_cast<AHATSource *>(&tcpStreamingSource);
     interactionSource = static_cast<InteractionSource *>(&tcpStreamingSource);
 
+//    discardDelayedFrames = false;
 //    fileDataSource = std::make_unique<FileDataSource>("/Users/liuzikai/Downloads/2022-11-01-201827-AHAT-PV-EYE-Green-Book-Slow");
 //    ahatSource = static_cast<AHATSource *>(fileDataSource.get());
 //    interactionSource = static_cast<InteractionSource *>(fileDataSource.get());
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 24; j++) {
+            BOTH_HANDS_EDGE_COLORS.row(i * 24 + j) = HAND_COLOR[i];
+        }
+    }
 
     igl::opengl::glfw::Viewer viewer;
     viewer.callback_pre_draw = callBackPerDraw;
     viewer.core().set_rotation_type(igl::opengl::ViewerCore::ROTATION_TYPE_TRACKBALL);
     viewer.data().point_size = 2;
+    viewer.data().line_width = 10;
     viewer.core().is_animating = true;
     Eigen::Vector4f color(1, 1, 1, 1);
-    viewer.core().background_color = color * 0.2f;
+    viewer.core().background_color = color * 0.0f;
     viewer.launch();
 }
