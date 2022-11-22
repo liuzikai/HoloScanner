2db63cef1f5e776599e7fc90b2cbcefbe479e2a5
Vellyxenya <noureddach@gmail.com>
2022-11-22 11:41:49 +0100

    Implement the registrator class

---
+++ CPP/registrator.cpp
@@ -0,0 +1,108 @@
+//
+// Created by Noureddine Gueddach on 21/11/2022.
+//
+
+#include "open3d/Open3D.h"
+#include "registrator.h"
+#include <memory>
+
+using namespace open3d;
+using std::cout;
+using std::endl;
+
+std::unique_ptr<PCD> Registrator::getReconstructedPCD() const {
+    PCD pcd;
+    pcd.reserve(m_pcd->points_.size());
+    for(const auto p : m_pcd->points_) {
+        pcd.push_back(p);
+    }
+    return std::make_unique<PCD>(pcd);
+}
+
+Eigen::Matrix4d Registrator::getTransformation(const geometry::PointCloud& source,
+    const geometry::PointCloud& target, Eigen::Matrix6d& InfoMat, 
+    const double kernel_param) {
+
+    int nb_iterations = 300;
+
+    double voxel_size = 0.02;
+    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+    for (int i = 0; i < 4; ++i) {
+        auto source_down = source.VoxelDownSample(voxel_size);
+        source_down->EstimateNormals(open3d::geometry::KDTreeSearchParamHybrid(
+            voxel_size * 2.0, 30));
+
+        auto target_down = target.VoxelDownSample(voxel_size);
+        target_down->EstimateNormals(open3d::geometry::KDTreeSearchParamHybrid(
+            voxel_size * 2.0, 30));
+
+        auto loss = pipelines::registration::TukeyLoss(kernel_param);
+        auto kernel = loss.k_;
+        auto result = pipelines::registration::RegistrationGeneralizedICP(
+            *source_down, *target_down, m_max_corr_dist_transformation, T,
+            pipelines::registration::TransformationEstimationForGeneralizedICP(kernel),
+            pipelines::registration::ICPConvergenceCriteria(1e-7, 1e-7, nb_iterations));
+        T = result.transformation_;
+        voxel_size /= 2;
+    }
+    InfoMat = pipelines::registration::GetInformationMatrixFromPointClouds(source, target, 
+        m_max_corr_dist_transformation, T);
+    return T;
+}
+
+bool Registrator::isRegistrationSuccessful(const geometry::PointCloud& pcd, const Eigen::Matrix4d& T) const {
+    auto result = pipelines::registration::EvaluateRegistration(pcd, *m_pcd, m_max_corr_dist_evaluation, T);
+    auto correspondance_set = result.correspondence_set_;
+    auto fitness = result.fitness_; //Corresponds to: correspondance_set.size() / pcd.points_.size()
+    auto rmse = result.inlier_rmse_;
+    //bool most_of_pcd_is_inlier = correspondance_set.size() >= 0.8 * pcd.points_.size(); //same as fitness
+    cout << fitness << " " << rmse << " " << endl;
+    bool high_fitness = fitness > m_min_fitness;
+    bool low_rmse = rmse < m_max_rmse;
+    return high_fitness && low_rmse;
+}
+
+bool Registrator::mergePCD(const PCD& pcd_) {
+    auto pcd = geometry::PointCloud(pcd_);
+    if(m_pcd == nullptr) { //First registration is always successful as it initializes the point cloud
+        m_pcd = std::make_shared<geometry::PointCloud>(pcd);
+        return true;
+    }
+
+    //DBSCAN
+    bool dbscan = false; //Note: DBSCAN is too slow for real-time (could be use) for a final pass though
+    if(dbscan) {
+        std::vector<int> indices = pcd.ClusterDBSCAN(0.1, 0.7 * pcd.points_.size());
+        PCD valid_points;
+        for(int i = 0; i < indices.size(); i++) {
+            if(indices[i] != -1)
+                valid_points.push_back(pcd.points_[i]);
+        }
+        pcd.points_ = valid_points;
+    }
+    
+    //Compute the transformation between the current and global point cloud
+    Eigen::Matrix6d InfoMat;
+    double kernel_param = 0.01;
+    Eigen::Matrix4d T = getTransformation(pcd, *m_pcd, InfoMat, kernel_param);
+
+    //Evaluate the registration
+    bool success = isRegistrationSuccessful(pcd, T);
+    //If not successful, keep the global point cloud as is, wait for the user to realign
+    if (!success) return false;
+
+    *m_pcd = m_pcd->Transform(T.inverse()); //Bring the global point cloud into the reference of the current frame
+    *m_pcd += pcd; //Merge the current frame to the global point cloud
+    m_pcd = m_pcd->VoxelDownSample(0.005); //downsample for performance
+
+    return true;
+}
+
+void Registrator::saveReconstructedMesh(const std::string& save_path) const {
+    std::shared_ptr<open3d::geometry::TriangleMesh> mesh;
+    std::vector<double> densities;
+    m_pcd->EstimateNormals();
+    float scale = 3;
+    std::tie(mesh, densities) = geometry::TriangleMesh::CreateFromPointCloudPoisson(*m_pcd, 8UL, 0, scale);
+    io::WriteTriangleMesh(save_path, *mesh);
+}
---
+++ CPP/registrator.h
@@ -0,0 +1,73 @@
+//
+// Created by Noureddine Gueddach on 21/11/2022.
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <string>
+#include <open3d/geometry/PointCloud.h>
+
+using PCD = std::vector<Eigen::Vector3d>; //Note: Open3D requires Vector3d, not Vector3f
+
+class Registrator {
+public:
+
+    //Using default params
+    Registrator() {}
+
+    //Using custom params
+    Registrator(float max_corr_dist_transformation, float max_corr_dist_evaluation, 
+        float max_rmse, float min_fitness) : 
+        m_max_corr_dist_transformation(m_max_corr_dist_transformation),
+        m_max_corr_dist_evaluation(max_corr_dist_evaluation),
+        m_max_rmse(max_rmse), m_min_fitness(min_fitness) { }
+
+    /**
+     * @brief Try to merge the given PCD into the global PCD. Returns whether the merge succeeds or not
+     * @param pcd [in] point to cloud to merge into the global point cloud
+     * @return true if the merge succeeds, false otherwise
+     */
+    bool mergePCD(const PCD& pcd);
+
+    /**
+     * @brief Returns the latest reconstruction result
+     * @return The point cloud reconstructed so far as a PCD type
+     */
+    std::unique_ptr<PCD> getReconstructedPCD() const;
+
+    /**
+     * @brief Construct a mesh from the point cloud and save it to disk
+     * @param save_path path to which the mesh should be saved
+     */
+    void saveReconstructedMesh(const std::string& save_path) const;
+
+private:
+    std::shared_ptr<open3d::geometry::PointCloud> m_pcd;
+    float m_max_corr_dist_transformation = 0.004;
+    float m_max_corr_dist_evaluation = 0.008;
+    float m_max_rmse = 0.004;
+    float m_min_fitness = 0.97;
+
+    /**
+     * @brief Construct a new is Registration Successful object
+     * 
+     * @param pcd new point cloud
+     * @param T transformation from new point cloud to global point cloud
+     */
+    bool isRegistrationSuccessful(const open3d::geometry::PointCloud& pcd, const Eigen::Matrix4d& T) const;
+
+    /**
+     * @brief Get the Transformation from the given point cloud to the target one
+     * 
+     * @param source new point cloud 
+     * @param target target point cloud
+     * @param InfoMat [out] 6x6 matrix holding transformation information about whatever
+     * @param kernel_param parameter used to indicate the stdev param of the TukeyLoss used internally
+     * @return [Eigen::Matrix4d] Transformation from source to target
+     */
+    Eigen::Matrix4d getTransformation(const open3d::geometry::PointCloud& source,
+        const open3d::geometry::PointCloud& target, Eigen::Matrix6d& InfoMat, 
+        const double kernel_param);
+};
