a8bf5fce10ed0ce06b1cda9fdee0577db2649bdf
Dude147 <dumeni.manatschal@gmail.com>
2022-11-23 16:21:14 +0100

    add statistical filter in DepthProcessor

--- CPP/DepthProcessor.cpp
+++ CPP/DepthProcessor.cpp
@@ -10,13 +10,25 @@ DepthProcessor::DepthProcessor(const DirectX::XMMATRIX &extrinsics, const float 
         : extrinsics(extrinsics), depthCameraPoseInvMatrix(XMMatrixInverse(nullptr, extrinsics)) {
 
     // Store LUT as XMVector
-    lut.reserve(AHAT_WIDTH * AHAT_HEIGHT);
-    for (size_t i = 0; i < AHAT_HEIGHT; i++) {
-        for (size_t j = 0; j < AHAT_WIDTH; j++) {
+    //lut.reserve(AHAT_WIDTH * AHAT_HEIGHT);
+    lut.reserve(clippedDepthFrameHeight * clippedDepthFrameWidth);
+    //for (size_t i = 0; i < AHAT_HEIGHT; i++) {
+    for (size_t i = roiRowLower; i < roiRowUpper; i++) {
+        //for (size_t j = 0; j < AHAT_WIDTH; j++) {
+        for (size_t j = roiColLower; j < roiColUpper; j++) {
             lut.emplace_back(XMLoadFloat3((const XMFLOAT3 *) lutBuf));
             lutBuf += 3;
         }
     }
+
+    // initialize std deviation back buffer
+    for (auto i = 0; i < stdLogSize; ++i) {
+        stdDepthVec.push_back(std::vector<float>(clippedDepthFrameWidth * clippedDepthFrameHeight), 0.0f);
+    }
+
+    depthMovingAverage.resize(clippedDepthFrameWidth * clippedDepthFrameHeight, 0.0f);
+    stdVal.resize(clippedDepthFrameWidth * clippedDepthFrameHeight, 0.0f);
+
 }
 
 bool DepthProcessor::getNextPCDRaw(timestamp_t &timestamp, PCDRaw &pcdRaw) {
@@ -28,24 +40,87 @@ bool DepthProcessor::getNextPCDRaw(timestamp_t &timestamp, PCDRaw &pcdRaw) {
     return true;
 }
 
-void DepthProcessor::updateAHAT(const timestamp_t &timestamp, const uint16_t *depthFrame,
-                                const DirectX::XMMATRIX &rig2world) {
+int DepthProcessor::countTrackedJoints(const Hand& hand) {
+    int result = 0;
+    for (const auto& joint : hand.joints) {
+        if (joint.tracked) result++;
+    }
+    return result;
+}
+
+void DepthProcessor::updateAHAT(
+    const timestamp_t &timestamp,
+    const uint16_t *depthFrame,
+    const DirectX::XMMATRIX &rig2world,
+    const InteractionFrame& hand
+    ) {
     std::pair<timestamp_t, PCDRaw> frame;
     frame.first = timestamp;
     frame.second.reserve((ROI_ROW_UPPER - ROI_ROW_LOWER) * (ROI_COL_UPPER - ROI_COL_LOWER));
 
     XMMATRIX depthToWorld = depthCameraPoseInvMatrix * rig2world;
 
-    for (auto i = ROI_ROW_LOWER; i < ROI_ROW_UPPER; i++) {
-        for (auto j = ROI_COL_LOWER; j < ROI_COL_UPPER; j++) {
-            auto idx = AHAT_WIDTH * i + j;
+    bool leftTracked = interactionFrame.hands[Left].tracked && countTrackedJoints(interactionFrame.hands[Left]);
+    bool rightTracked = interactionFrame.hands[Right].tracked && countTrackedJoints(interactionFrame.hands[Right]);
+    if (!leftTracked || !rightTracked) return; //lostTracking = true;
+
+    DirectX::XMMATRIX leftWrist = hand.hands->joints[HandIndex::Left][HandJointIndex::Wrist].transformation;
+    DirectX::XMMATRIX rightWrist = hand.hands->joints[HandIndex::Right][HandJointIndex::Wrist].transformation;
+
+    auto lwTranf = XMTransformToTranslate(leftWrist);
+    auto rwTransf = XMTransformToTranslate(rightWrist);
+    auto midpoint = XMVectorMidpoint(leftWrist, rightWrist);
+
+    uint16_t midpZ = static_cast<uint16_t>(midpoint[2] * 1000);
+    uint16_t depthNearClip = static_cast<uint16_t>(midpZ - 200.0f); // 200 == 200cm
+    uint16_t depthFarClip = static_cast<uint16_t>(midpZ + 200.0f);
+
+    //self.window__cp_x_from = 256 - 168
+    //self.window__cp_x_to = 256 + 80
+    //self.window__cp_y_from = 256 - 128
+    //self.window__cp_y_to = 256 + 128
+
+
+    size_t bufferInd = stdLogIndex % stdLogSize;
+    size_t nextInd = (stdLogIndex + 1) % stdLogSize;
+    for (auto i = roiRowLower; i < rowRowUpper; i++) {
+        for (auto j = roiColLower; j < roiColUpper; j++) {
+            //auto idx = AHAT_WIDTH * i + j;
             uint16_t depth = depthFrame[idx];
             depth = (depth > 4090) ? 0 : depth;
 
-            if (depth > DEPTH_NEAR_CLIP && depth < DEPTH_FAR_CLIP) {
+            size_t ind = i * clippedDepthFrameHeight + j;
+            stdDepthVec[bufferInd][ind] = static_cast<float>(depth);
+            depthMovingAverage[ind] -= stdDepthVec[nextInd][ind];
+            depthMovingAverage[ind] += depth;
+        }
+    }
 
-                // optimize with LUT
-                auto tempPoint = (float) depth / 1000 * lut[idx];
+    float s = static_cast<float>(stdLogSize);
+    float curMaxStdVal = std::numeric_limits<float>::min();
+    for (auto r = 0; r < clippedDepthFrameHeight; ++r) {
+        for (auto c = 0; c < clippedDepthFrameWidth; ++c) {
+            size_t ind = r * clippedDepthFrameHeight + c;
+            float dev = 0;
+            for (auto b = 0; b < stdLogSize; ++d) {
+                dev += std::powf((stdDepthVec[b][ind] - depthMovingAverage[ind] / s), 2.0f);
+            }
+
+            stdVal[ind] = dev;
+            if (dev > curMaxStdVal) {
+                curMaxStdVal = dev;
+            }
+        }
+    }
+
+    for (auto r = 0; r < clippedDepthFrameHeight; ++r) {
+        for (auto c = 0; c < clippedDepthFrameWidth; ++c) {
+            size_t ind = r * clippedDepthFrameHeight + c;
+
+            float depth = stdDepthVec[bufferInd][ind];
+            float cDev = stdVal[ind] / curMaxStdVal;
+            if (cDev < maxStdVal && depth > depthNearClip && depth < depthFarClip) {
+                auto tempPoint = depth / 1000 * lut[ind];
 
                 // apply transformation
                 auto pointInWorld = XMVector3Transform(tempPoint, depthToWorld);
--- CPP/DepthProcessor.h
+++ CPP/DepthProcessor.h
@@ -5,21 +5,23 @@
 #ifndef HOLOSCANNER_DEPTHPROCESSOR_H
 #define HOLOSCANNER_DEPTHPROCESSOR_H
 
-#include "DepthDataTypes.h"
 #include <queue>
 #include <mutex>
+
+#include "DepthDataTypes.h"
+#include "InteractionDataTypes.h"
 
 class DepthProcessor {
 public:
 
     DepthProcessor(const DirectX::XMMATRIX &extrinsics, const float *lut);
 
-    void updateAHAT(const timestamp_t &timestamp, const uint16_t *depth, const DirectX::XMMATRIX &rig2world);
+    void updateAHAT(const timestamp_t &timestamp, const uint16_t *depth, const DirectX::XMMATRIX &rig2world, const InteractionFrame &hand);
 
     bool getNextPCDRaw(timestamp_t &timestamp, PCDRaw &pcdRaw);
 
 private:
-
+    int countTrackedJoints(const Hand& hand);
     static constexpr size_t ROI_ROW_LOWER = (size_t) (0.2 * AHAT_HEIGHT);
     static constexpr size_t ROI_ROW_UPPER = (size_t) (0.55 * AHAT_HEIGHT);
     static constexpr size_t ROI_COL_LOWER = (size_t) (0.3 * AHAT_WIDTH);
@@ -27,6 +29,19 @@ private:
     static constexpr uint16_t DEPTH_NEAR_CLIP = 200; // Unit: mm
     static constexpr uint16_t DEPTH_FAR_CLIP = 800;
 
+    constexpr size_t roiColLower = 256 - 128;
+    constexpr size_t roiColUpper = 256 + 128;
+    constexpr size_t roiRowLower = 256 - 168;
+    constexpr size_t roiRowUpper = 256 + 80;
+    constexpr size_t clippedDepthFrameWidth = roiColUpper - roiColLower;
+    constexpr size_t clippedDepthFrameHeight = roiRowUpper - roiRowLower;
+
+    size_t stdLogIndex = 0;
+    constexpr size_t stdLogSize = 10;
+    constexpr float maxStdVal = 0.01*0.01; // squared value on purpose
+    std::vector<float> stdVal;
+    std::vector<float> depthMovingAverage;
+    std::vector<std::vector<float>> stdDepthVec;
     std::queue<std::pair<timestamp_t, PCDRaw>> pcdRawFrames;
     std::mutex pcdMutex;
 
